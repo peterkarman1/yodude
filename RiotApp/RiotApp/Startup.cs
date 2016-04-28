b494a5238160e618de581ebe4537292e056b21b8
Peter Karman <Peter Karman>
2016-04-28 19:42:23 -0400

    knitial push

---
+++ RiotApp/RiotApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RiotApp.Startup))]
+namespace RiotApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
