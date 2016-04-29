a9d512a76dee0c52d3349a04acf504fa98b7a8d5
Peter Karman <Peter Karman>
2016-04-28 20:50:59 -0400

    add some calls

--- RiotApp/RiotApp/Controllers/ChampionMasteryController.cs
+++ RiotApp/RiotApp/Controllers/ChampionMasteryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Web.Http;
 using System.Web.Mvc;
+using RiotApp.Models;
 
 namespace RiotApp.Controllers
 {
@@ -11,13 +12,37 @@ namespace RiotApp.Controllers
     {
         private readonly ApiClient _client = new ApiClient();
         private const string _key = "somekey";
-
+        private string[] _regions = {"na","jp","ru"};
+        private string _replaceable = "replacemehomie";
         
-        public JsonResult GetMasteryInfo(string summonerName, string championId)
+        public ChampionMastery GetMasteryInfo(string summonerId, string championId)
         {
-            var url =
+            var url = $"https://na.api.pvp.net/api/lol//championmastery/location/{_replaceable}/player/{summonerId}/champion/{championId}?api_key={_key}";
+            return CycleThroughRegionsAndReturnFirstResult<ChampionMastery>(url);
         }
 
-        
+        public Summoner GetSummonerInfo(string summonerName)
+        {
+            string formattedSummoner = summonerName.ToLower().Replace(" ", "").Trim();
+            var url = $"https://na.api.pvp.net/api/lol/{_replaceable}/v1.4/summoner/by-name/{formattedSummoner}";
+            return CycleThroughRegionsAndReturnFirstResult<Summoner>(url);
+        }
+
+        private T CycleThroughRegionsAndReturnFirstResult<T>(string url) where T : class, new()
+        {
+            var model  = new T();
+            foreach (var region in _regions)
+            {
+                var replacedurl = url.Replace(_replaceable, region);
+                var result = _client.GetAsync<T>(url).Result;
+                if (result != null)
+                {
+                    model = result;
+                    break;
+                }
+            }
+
+            return model;
+        }
     }
 }
