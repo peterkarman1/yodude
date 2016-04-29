a922937e8836b483c479315f5bd3ffaf5386fb41
Peter Karman <Peter Karman>
2016-04-28 20:25:24 -0400

    format json

--- RiotApp/RiotApp/Controllers/ChampionMasteryController.cs
+++ RiotApp/RiotApp/Controllers/ChampionMasteryController.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
+using System.Web.Mvc;
 
 namespace RiotApp.Controllers
 {
     public class ChampionMasteryController : ApiController
     {
+        private readonly ApiClient _client = new ApiClient();
+        private const string _key = "somekey";
+
+        
+        public JsonResult GetMasteryInfo(string summonerName, string championId)
+        {
+            var url =
+        }
+
+        
     }
 }
