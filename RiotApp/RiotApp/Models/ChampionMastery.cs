0b78e4819c5a3e1c1fde08748901252ec44a5074
Zach Holt <Zach Holt>
2016-04-28 20:23:03 -0400

    added models

---
+++ RiotApp/RiotApp/Models/ChampionMastery.cs
@@ -0,0 +1,15 @@
+﻿namespace RiotApp.Models
+{
+    public class ChampionMastery
+    {
+        public long PlayerId { get; set; }
+        public int ChampionId { get; set; }
+        public int ChampionPoints { get; set; }
+        public int ChampionLevel { get; set; }
+        public int ChampionPointsUntilNextLevel { get; set; }
+        public int ChampionPointsSinceLastLevel { get; set; }
+        public string HighestGrade { get; set; }
+        public bool ChestGranted { get; set; }
+        public string LastPlayTime { get; set; }
+    }
+}
---
+++ RiotApp/RiotApp/Models/Summoner.cs
@@ -0,0 +1,11 @@
+﻿namespace RiotApp.Models
+{
+    public class Summoner
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int ProfileIconId { get; set; }
+        public long RevisionDate { get; set; }
+        public int SummonerLevel { get; set; }
+    }
+}
