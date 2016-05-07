276a29f23f92a485d49d47c6375190ba63b8e997
Peter Karman <Peter Karman>
2016-05-07 18:05:01 -0400

    add mock challeneg

--- RiotApp/RiotApp/Controllers/ChallengeController.cs
+++ RiotApp/RiotApp/Controllers/ChallengeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using RiotApp.Models;
 
 namespace RiotApp.Controllers
@@ -15,6 +16,34 @@ namespace RiotApp.Controllers
         public ActionResult Submit(ChallengeSubmission submission)
         {
             return submission.Points > 10000 ? new HttpStatusCodeResult(500) : new HttpStatusCodeResult(200);
+        }
+        //Mock data for challenge first to 5000 points
+        [HttpPost]
+        public ActionResult GetChallengeProgress(int challengeId)
+        {
+            ChallengeSubmission player1 = GetPlayer1Model(challengeId);
+            ChallengeSubmission player2 = GetPlayer2Model(challengeId);
+            return Json(new {player1 = player1, player2 = player2, challengeComplete = challengeId == 1 , championId = challengeId == 1 ? 266 : 267});
+        }
+
+        private ChallengeSubmission GetPlayer2Model(int id)
+        {
+            return new ChallengeSubmission
+            {
+                EndDate = id == 1 ? DateTime.Now : DateTime.Today.AddDays(1),
+                Points = id == 1 ? 4587 : 2300,
+                SummonerId = 1
+            };
+        }
+
+        private ChallengeSubmission GetPlayer1Model(int id)
+        {
+            return new ChallengeSubmission
+            {
+                EndDate = id == 1 ? DateTime.Now : DateTime.Today.AddDays(1),
+                Points = id == 1 ? 5000 : 2345,
+                SummonerId = 1
+            };
         }
     }
 }
